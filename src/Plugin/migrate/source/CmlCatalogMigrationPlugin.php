9f97cf3c7975f0ba3ea1122ee7c0f94bb4291046
Anatoly Politsin <politsin@gmail.com>
2017-08-29 15:07:02 +0300

    prepare

--- src/Plugin/migrate/source/CmlCatalogMigrationPlugin.php
+++ src/Plugin/migrate/source/CmlCatalogMigrationPlugin.php
@@ -3,6 +3,10 @@
 namespace Drupal\cmlmigrations\Plugin\migrate\source;
 
 use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+use Drupal\cmlservice\Xml\CatalogParcer;
+use Drupal\cmlservice\Controller\GetLastCml;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\cmlservice\Xml\XmlObject;
 
 /**
  * Source for CSV.
@@ -12,6 +16,51 @@ use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
  * )
  */
 class CmlCatalogMigrationPlugin extends SourcePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+    $cml = GetLastCml::load();
+    $cml_xml = $cml->field_cml_xml->getValue();
+    $files = [];
+    $data = FALSE;
+    $filekeys['import'] = TRUE;
+    if (!empty($cml_xml)) {
+      foreach ($cml_xml as $xml) {
+        $file = file_load($xml['target_id']);
+        $filename = $file->getFilename();
+        $filekey = strstr($filename, '.', TRUE);
+        if (isset($filekeys[$filekey]) && $filekeys[$filekey]) {
+          $files[] = $file->getFileUri();
+        }
+      }
+    }
+    $filepath = array_shift($files);
+    if ($filepath) {
+      $xmlObj = new XmlObject();
+      $xmlObj->parseXmlFile($filepath);
+      $data = CatalogParcer::parce($xmlObj->xmlString);
+    }
+
+    $this->rows = $data;
+    dsm($data);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function initializeIterator() {
+    return new \ArrayIterator([['id' => '']]);
+  }
+
+  /**
+   * Allows class to decide how it will react when it is treated like a string.
+   */
+  public function __toString() {
+    return '';
+  }
 
   /**
    * {@inheritdoc}
@@ -30,17 +79,22 @@ class CmlCatalogMigrationPlugin extends SourcePluginBase {
    */
   public function fields() {
     $fields = [];
-    $xml = $fetcher_result->getRaw();
-    $raws  = CatalogParcer::parce($xml);
-    foreach ($raws as $key => $raw) {
+    foreach ($this->rows as $key => $row) {
       $fields[$key] = [
-        'uuid' => $raw['id'],
-        'name' => $raw['name'],
+        'uuid' => $row['id'],
+        'name' => $row['name'],
         'weight' => $raw['term_weight'],
-        'parent_uuid' => [trim($raw['parent'])],
+        'parent_uuid' => [trim($row['parent'])],
       ];
     }
     return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function count() {
+    return count($this->rows);
   }
 
 }
