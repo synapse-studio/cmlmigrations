15b905e1170611d82c04835bdad58de43c7f29cd
Anatoly Politsin <politsin@gmail.com>
2017-08-29 12:46:39 +0300

    first commit

---
+++ src/Plugin/migrate/source/CmlCatalogMigrationPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\cmlmigrations\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+
+/**
+ * Source for CSV.
+ *
+ * @MigrateSource(
+ *   id = "cml_catalog"
+ * )
+ */
+class CmlCatalogMigrationPlugin extends SourcePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIDs() {
+    return [
+      'uuid' => [
+        'type' => 'string',
+        'alias' => 'uuid',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'uuid' => $this->t('1С UUID'),
+      'name' => $this->t('Name'),
+      'weight' => $this->t('Term weight'),
+      'parent_uuid' => $this->t('Parent UUID'),
+    ];
+    return $fields;
+  }
+
+}
